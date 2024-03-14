c12b437d0dff6abafeb65bbd1307220d7c38e99b
Adailton Braga Júnior <adailtonjn68@gmail.com>
2024-03-14 15:15:38 -0300

    Added section in build.sh to install

--- build.sh
+++ build.sh
@@ -10,6 +10,8 @@ C_FLAGS="${WARNINGS} ${STANDARD} ${OPTIM} ${DEBUG}"
 
 TEST_DIR=./Tests
 BUILD_DIR=./Build
+INSTALL_DIR=/usr/local
+
 
 if [[ $1 == "clear" ]]; then
     rm -f ${BUILD_DIR}/*
@@ -17,6 +19,11 @@ elif [[ $1 == "compile" ]]; then
     ${CC} ${C_FLAGS} pso.c ${TEST_DIR}/test01.c -o ${BUILD_DIR}/test01
 elif [[ $1 == "run" ]]; then
     {BUILD_DIR}/test01
+elif [[ $1 == "install" ]]; then
+    ${CC} ${C_FLAGS} pso.c -fPIC -shared -o libpso.so
+    mv libpso.so ${INSTALL_DIR}/lib/
+    cp pso.h ${INSTALL_DIR}/include/
+    ldconfig
 else
     echo "Pass one of the following arguments"
     echo -e "\tclear\n\tcompile\n\ttests\n\texamples"
